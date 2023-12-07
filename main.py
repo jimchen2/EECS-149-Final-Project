7245300d3d3bbc8b4944e21af5e7dfa77beb33cd
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2023-12-06 22:13:38 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-import subprocess
-import os
-import torch
+from ultralytics import YOLO
 
-# Path to your YOLOv5 directory
-yolov5_dir = '/home/pi/yolov5-rasp-pi'
+model = YOLO('yolov5n.pt')
+results = model('tcp://127.0.0.1:8888', stream=True)
 
-# Start the libcamera-vid command
-libcamera_cmd = [
-    'libcamera-vid', '-n', '-t', '0',
-    '--width', '1280', '--height', '960',
-    '--framerate', '1', '--inline', '--listen',
-    '-o', 'tcp://127.0.0.1:8888'
-]
-subprocess.Popen(libcamera_cmd)
-
-# Load the YOLOv5 model
-weights_path = os.path.join(yolov5_dir, 'best.pt')
-model = torch.hub.load('ultralytics/yolov5', 'custom', path=weights_path)
-
-# Define the source
-source = 'tcp://127.0.0.1:8888'
-
-# Perform detection
-results = model(source)
-
-# Process results to print bounding box information
-for result in results.xyxy[0]:
-    # Each result contains [x1, y1, x2, y2, confidence, class]
-    x1, y1, x2, y2, conf, cls = result
-    print(f"Bounding Box: x1={x1}, y1={y1}, x2={x2}, y2={y2}, Confidence: {conf}, Class: {cls}")
+while True:
+    for result in results:
+        boxes = result.boxes
+        probs = result.probs
