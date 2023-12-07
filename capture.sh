e971bee4460851c484e1264e8d2c38fc56f76aae
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2023-12-07 03:03:44 -0800

    Update capture.sh

--- capture.sh
+++ capture.sh
@@ -1,4 +1,4 @@
 libcamera-vid -n -t 0 --width 1280 --height 960 --framerate 1 --inline --listen -o tcp://127.0.0.1:9999
 
 
-python3 detect.py --source=tcp://127.0.0.1:9999 --weights /home/pi/best.pt
+mypython detect.py --source=tcp://127.0.0.1:9999 --weights /home/pi/best.pt
