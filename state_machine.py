32412c15fbba7893f0dced631899b20840119a21
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2023-12-06 18:42:08 -0800

    Update state_machine.py

--- state_machine.py
+++ state_machine.py
@@ -1,66 +1,56 @@
 import detect_line
 import yolov5_trash
 import PID_function
+
 class RobotStateMachine:
     def __init__(self):
-        # Start in the Navigation Mode state
         self.state = "Navigation Mode"
 
-    # Define a method for each state in the state diagram
-
     def navigation_mode(self):
-        # Implement navigation logic here
-        print("Robot is in Navigation Mode, searching for trash or avoiding obstacles.")
-        # Check sensors and switch state based on conditions
+        print("Robot is in Navigation Mode.")
         if self.detect_trash():
             self.state = "Trash Picking Mode"
         elif self.detect_error():
             self.state = "Error Handling"
-        line_postion=detect_line()
-        motor_input=PID_function(line_position)
-        motor_move(motor_input)
-
-        # ... more conditions based on your state diagram
+        else:
+            line_position = detect_line.detect()
+            motor_input = PID_function.calculate(line_position)
+            self.motor_move(motor_input)
 
     def error_handling(self):
-        # Implement error handling logic here
-        print("Robot is handling an error.")
-        # Once the error is resolved, return to Navigation Mode
+        print("Handling error.")
+        # Implement actual error handling logic here
         self.state = "Navigation Mode"
 
     def trash_picking_mode(self):
-        # Implement trash detection and picking logic here
-        print("Robot is in Trash Picking Mode, trying to pick up trash.")
-        # Once trash is centered, switch to Picking Up Mode
+        print("Robot is in Trash Picking Mode.")
         self.state = "Picking Up Mode"
 
     def picking_up_mode(self):
-        # Implement picking up trash logic here
         print("Robot is picking up trash.")
-        # Once trash is collected, check for more trash or return to Navigation Mode
         if self.more_trash_detected():
             self.state = "Trash Picking Mode"
         else:
             self.state = "Navigation Mode"
 
-    # Define methods to simulate sensor input for demonstration purposes
-
     def detect_trash(self):
-        # Simulate trash detection
-        return True
+        return yolov5_trash.detect()
 
     def detect_error(self):
-        # Simulate error detection
+        # Implement actual error detection logic
         return False
 
-    # Main loop to run the state machine
+    def more_trash_detected(self):
+        # Implement logic to detect more trash
+        return False
+
+    def motor_move(self, input):
+        # Implement motor movement logic
+        pass
+
     def run(self):
         while True:
-            # Call the method corresponding to the current state
             getattr(self, self.state.lower().replace(" ", "_"))()
-            # Break the loop after one cycle for demonstration purposes
-            break
 
-# Instantiate and run the state machine
 robot = RobotStateMachine()
 robot.run()
