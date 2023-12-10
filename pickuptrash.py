c02fa5e9fd54a3f0b42dfd52f441a5b66499a92c
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2023-12-09 23:35:54 -0800

    Create pickuptrash.py

---
+++ pickuptrash.py
@@ -0,0 +1,28 @@
+import time
+import Adafruit_PCA9685
+
+# Initialize the PCA9685 using the default address (0x40) and bus number 1.
+pwm = Adafruit_PCA9685.PCA9685(busnum=1)
+
+def pickup_trash():
+    """
+    Function to control a robotic claw to pick up trash.
+    """
+
+    # Step 1: Open the claw.
+    # The set_pwm method takes three arguments: channel, on, and off.
+    # Channel 15 controls the claw's opening and closing.
+    pwm.set_pwm(15, 0, 100)
+
+    # Step 2: Push the claw down.
+    # Channel 12 controls the claw's up and down movement.
+    pwm.set_pwm(12, 0, 170)
+    
+    # Wait for 2 seconds to ensure the claw has reached the trash.
+    time.sleep(2)
+
+    # Step 3: Close the claw to grab the trash.
+    pwm.set_pwm(15, 0, 250)
+
+    # Step 4: Lift the claw up.
+    pwm.set_pwm(12, 0, 60)
