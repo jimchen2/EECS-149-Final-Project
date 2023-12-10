0f5103c027f56546f7a7f6bfb50297a04b22beb0
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2023-12-10 01:04:46 -0800

    Update trashrecognition.py

--- trashrecognition.py
+++ trashrecognition.py
@@ -1,28 +1,45 @@
-from ultralytics import YOLO
-import subprocess
-import threading
+import torch
+from torchvision import transforms
+import cv2
+import sys
 
-# Function to run the libcamera-vid command
-def run_camera():
-    cmd = "libcamera-vid -n -t 0 --width 1280 --height 960 --framerate 1 --inline --listen -o tcp://127.0.0.1:8888"
-    subprocess.run(cmd, shell=True)
+# Load the YOLOv5 model
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='/home/pi/best.pt')
 
-# Start the camera capture in a separate thread
-camera_thread = threading.Thread(target=run_camera)
-camera_thread.start()
+# Append path to sys
+sys.path.append('/home/pi/EECS-149-Final-Project/scripts')
 
-# Load YOLO model
-model = YOLO('yolov8n.pt')
+# Import the function to capture numpy image
+from capture_numpy_image import capture_numpy_image
 
-# Process video stream
-results = model('tcp://127.0.0.1:8888', stream=True)
+# Capture the image
+numpy_image = capture_numpy_image(timeout=1000, resolution=(640, 480), preview=False, hflip=True)
 
-while True:
-    for result in results:
-        # Extract boxes and probabilities
-        boxes = result.boxes
-        probs = result.probs
+# Convert BGR (OpenCV default) to RGB if necessary
+numpy_image_rgb = cv2.cvtColor(numpy_image, cv2.COLOR_BGR2RGB)
 
-        # Print boxes and probabilities
-        print("Boxes:", boxes)
-        print("Probabilities:", probs)
+# Convert numpy image to PyTorch tensor
+tensor_image = transforms.ToTensor()(numpy_image_rgb).unsqueeze_(0)
+
+# Get predictions from the model
+predictions = model(tensor_image)
+
+# Process the predictions
+for detection in predictions[0]:
+    # Extract information from the detection
+    x_center, y_center, width, height, confidence, class_id = detection
+
+    # Convert center coordinates (x_center, y_center) and size (width, height) to corner coordinates (xmin, ymin, xmax, ymax)
+    xmin = x_center - (width / 2)
+    ymin = y_center - (height / 2)
+    xmax = x_center + (width / 2)
+    ymax = y_center + (height / 2)
+
+    print(f"Object Detected: Class ID - {class_id}, Confidence - {confidence:.2f}")
+    print(f"Bounding Box Coordinates: xmin - {xmin}, ymin - {ymin}, xmax - {xmax}, ymax - {ymax}")
+
+    # Draw bounding boxes on the image
+    cv2.rectangle(numpy_image_rgb, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 2)
+
+# At this point, you can display the image using cv2.imshow() or save it using cv2.imwrite(), 
+# depending on your requirements.
