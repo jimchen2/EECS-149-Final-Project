eaf451460eb24282640e7ca1af83ebcbc9f75e8a
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2023-12-10 01:08:07 -0800

    Update trashrecognition.py

--- trashrecognition.py
+++ trashrecognition.py
@@ -24,22 +24,27 @@ tensor_image = transforms.ToTensor()(numpy_image_rgb).unsqueeze_(0)
 # Get predictions from the model
 predictions = model(tensor_image)
 
+# Define a confidence threshold (e.g., 0.25)
+confidence_threshold = 0.25
+
 # Process the predictions
 for detection in predictions[0]:
     # Extract information from the detection
     x_center, y_center, width, height, confidence, class_id = detection
 
-    # Convert center coordinates (x_center, y_center) and size (width, height) to corner coordinates (xmin, ymin, xmax, ymax)
-    xmin = x_center - (width / 2)
-    ymin = y_center - (height / 2)
-    xmax = x_center + (width / 2)
-    ymax = y_center + (height / 2)
+    # Check if the confidence is above the threshold
+    if confidence >= confidence_threshold:
+        # Convert center coordinates (x_center, y_center) and size (width, height) to corner coordinates (xmin, ymin, xmax, ymax)
+        xmin = x_center - (width / 2)
+        ymin = y_center - (height / 2)
+        xmax = x_center + (width / 2)
+        ymax = y_center + (height / 2)
 
-    print(f"Object Detected: Class ID - {class_id}, Confidence - {confidence:.2f}")
-    print(f"Bounding Box Coordinates: xmin - {xmin}, ymin - {ymin}, xmax - {xmax}, ymax - {ymax}")
+        print(f"Object Detected: Class ID - {class_id}, Confidence - {confidence:.2f}")
+        print(f"Bounding Box Coordinates: xmin - {xmin}, ymin - {ymin}, xmax - {xmax}, ymax - {ymax}")
 
-    # Draw bounding boxes on the image
-    cv2.rectangle(numpy_image_rgb, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 2)
+        # Draw bounding boxes on the image
+        cv2.rectangle(numpy_image_rgb, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 2)
 
 # At this point, you can display the image using cv2.imshow() or save it using cv2.imwrite(), 
 # depending on your requirements.
